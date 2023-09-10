51469bf758880b60d58216e7d7cd2eb08dc2952e
Muhammad1831 <muhammadsaleh1356@gmail.com>
2023-09-10 13:53:28 -0700

    create string class for manage const string

---
+++ lib/my_string.dart
@@ -0,0 +1,6 @@
+class myString {
+  static const String viewHottestPosts = 'مشاهده داغ ترین نوشته ها';
+  static const String viewHottestPodcasts = 'مشاهده داغ ترین پادکست ها';
+  static const String viewHotestBlog = 'مشاهده داغ ترین نوشته ها';
+  static const String relatedPosts = 'نوشته های مرتبط';
+}
