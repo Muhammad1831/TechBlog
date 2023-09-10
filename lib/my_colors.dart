af851af152b3a4444d0164585614cb505346c128
Muhammad1831 <muhammadsaleh1356@gmail.com>
2023-09-10 13:51:02 -0700

    create color class for manage text colors

---
+++ lib/my_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SolidColors {
+  static const Color textTitle = Color.fromARGB(255, 0, 0, 0);
+  static const Color lightText = Color.fromARGB(255, 255, 255, 255);
+  static const Color scafoldBackground = Color.fromARGB(255, 255, 255, 255);
+  static const Color selectedPodcast = Color.fromARGB(255, 255, 139, 26);
+  static const Color categoryColor = Color.fromARGB(255, 44, 125, 180);
+}
+
+class GradientColors {
+  static const List<Color> buttonNavigationGradient = [
+    Color.fromARGB(255, 25, 0, 94),
+    Color.fromARGB(255, 68, 4, 87)
+  ];
+
+  static const List<Color> tag = [
+    Color.fromARGB(255, 255, 255, 255),
+    Color.fromARGB(255, 0, 0, 0)
+  ];
+}
