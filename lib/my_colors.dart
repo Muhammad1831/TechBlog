eb62f9a8d6475d3eacbb877cbcc10e9b968f0c1c
Muhammad1831 <muhammadsaleh1356@gmail.com>
2023-09-14 02:16:30 -0700

    add new color and string

--- lib/my_colors.dart
+++ lib/my_colors.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class SolidColors {
+  static const Color primaryColor = Color.fromARGB(255, 68, 4, 87);
   static const Color textTitle = Color.fromARGB(255, 0, 0, 0);
+  static const Color menuIcon = Color.fromARGB(255, 0, 0, 0);
+  static const Color searchIcon = Color.fromARGB(255, 0, 0, 0);
   static const Color lightText = Color.fromARGB(255, 255, 255, 255);
+  static const Color statusBarColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color systemNavigationBarColor = Color.fromARGB(255, 255, 255, 255);
   static const Color scafoldBackground = Color.fromARGB(255, 255, 255, 255);
+  static const Color posterHomePageTitle = Color.fromARGB(255, 255, 255, 255);
+  static const Color posterHomePageSubTitle = Color.fromARGB(204, 255, 255, 255);    
   static const Color selectedPodcast = Color.fromARGB(255, 255, 139, 26);
   static const Color categoryColor = Color.fromARGB(255, 44, 125, 180);
 }
@@ -15,7 +22,13 @@ class GradientColors {
   ];
 
   static const List<Color> tag = [
-    Color.fromARGB(255, 255, 255, 255),
-    Color.fromARGB(255, 0, 0, 0)
+    Color.fromARGB(255, 60, 60, 60),
+    Color.fromARGB(255, 6, 6, 6)
+  ];
+
+  static const List<Color> posterCoverHomePageGradient = [
+    Color.fromARGB(0, 40, 41, 46),
+    Color.fromARGB(178, 72, 20, 88),
+    Color.fromARGB(255, 28, 20, 81)
   ];
 }
--- lib/my_string.dart
+++ lib/my_string.dart
@@ -1,4 +1,4 @@
-class myString {
+class MyString {
   static const String viewHottestPosts = 'مشاهده داغ ترین نوشته ها';
   static const String viewHottestPodcasts = 'مشاهده داغ ترین پادکست ها';
   static const String viewHotestBlog = 'مشاهده داغ ترین نوشته ها';
