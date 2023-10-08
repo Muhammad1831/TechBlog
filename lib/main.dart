c1bca2ad042a6c5b088614e8eb16b0671b88ea5b
Muhammad1831 <muhammadsaleh1356@gmail.com>
2023-10-08 21:44:11 +0330

    add inputDecorationTheme & elevatedButtonTheme in ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:techblog/my_colors.dart';
-import 'package:techblog/view/splash_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:techblog/view/splash_screen.dart';
 
 void main() {
   //setting the color of the icons and the background of the status bar and the navigator system
@@ -20,6 +20,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
@@ -32,40 +34,92 @@ class MyApp extends StatelessWidget {
       ],
 
       theme: ThemeData(
+          // Theme used in application text fields
+          inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(width: 1.5)),
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(width: 1.5)),
+              hintStyle: const TextStyle(
+                  fontFamily: 'iran',
+                  fontSize: 14,
+                  color: SolidColors.hintText,
+                  fontWeight: FontWeight.w300),
+              filled: true,
+              fillColor: SolidColors.textFieldColor),
+          // the Theme used in the application buttons
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              fixedSize: MaterialStatePropertyAll(
+                  Size(size.width / 2.5, size.height / 15.4)),
+              shape: MaterialStateProperty.all(const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              )),
+              backgroundColor: MaterialStateProperty.resolveWith(
+                (states) {
+                  if (states.contains(MaterialState.pressed)) {
+                    return SolidColors.pencilIcon;
+                  }
+                  return SolidColors.primaryColor;
+                },
+              ),
+              textStyle: MaterialStateProperty.resolveWith((states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return const TextStyle(
+                      fontFamily: 'iran',
+                      fontSize: 20,
+                      color: SolidColors.posterHomePageTitle,
+                      fontWeight: FontWeight.w700);
+                }
+                return const TextStyle(
+                    fontFamily: 'iran',
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300);
+              }),
+            ),
+          ),
           textTheme: const TextTheme(
-        headline1: TextStyle(
-            fontFamily: 'iran',
-            fontSize: 20,
-            color: SolidColors.posterHomePageTitle,
-            fontWeight: FontWeight.w700),
-        headline2: TextStyle(
-            fontFamily: 'iran',
-            fontSize: 14,
-            color: Colors.white,
-            fontWeight: FontWeight.w300),
-        headline3: TextStyle(
-            fontFamily: 'iran',
-            fontSize: 14,
-            color: SolidColors.myStringClass,
-            fontWeight: FontWeight.w700),
-        headline4: TextStyle(
-            fontFamily: 'iran',
-            fontSize: 14,
-            color: SolidColors.textTitle,
-            fontWeight: FontWeight.w700),
-        subtitle1: TextStyle(
-            fontFamily: 'iran',
-            fontSize: 14,
-            color: SolidColors.posterHomePageSubTitle,
-            fontWeight: FontWeight.w300),
-        bodyText1: TextStyle(
-            fontFamily: 'iran',
-            fontSize: 20,
-            color: Color.fromARGB(255, 72, 72, 72),
-            fontWeight: FontWeight.w300),
-      )),
+            headline1: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 20,
+                color: SolidColors.posterHomePageTitle,
+                fontWeight: FontWeight.w700),
+            headline2: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.w300),
+            headline3: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 14,
+                color: SolidColors.myStringClass,
+                fontWeight: FontWeight.w700),
+            bodyText1: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 14,
+                color: SolidColors.textTitle,
+                fontWeight: FontWeight.w700),
+            headline4: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 14,
+                color: SolidColors.posterHomePageSubTitle,
+                fontWeight: FontWeight.w300),
+            headline5: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 14,
+                color: SolidColors.hintText,
+                fontWeight: FontWeight.w300),
+            bodyText2: TextStyle(
+                fontFamily: 'iran',
+                fontSize: 20,
+                color: Color.fromARGB(255, 72, 72, 72),
+                fontWeight: FontWeight.w300),
+          )),
       //go to the splash screen
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
