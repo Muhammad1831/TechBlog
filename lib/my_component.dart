c6ba5a9a8361c90425c35c9fb18ff91b9a17cc33
Muhammad1831 <muhammadsaleh1356@gmail.com>
2023-10-08 21:30:37 +0330

    Update

--- lib/my_component.dart
+++ lib/my_component.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:techblog/my_colors.dart';
 
+// is a widget that creates a dividing line
 class TechBlogDivider extends StatelessWidget {
   const TechBlogDivider({
     super.key,
@@ -15,7 +16,7 @@ class TechBlogDivider extends StatelessWidget {
       color: SolidColors.dividerColor,
       indent: size.width / 4.7,
       endIndent: size.width / 4.7,
-      thickness: 0.4,
+      thickness: 1,
     );
   }
 }
