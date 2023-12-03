aa0d4bb4912dd70a0baafc8192ac73783fe522ec
Muhammad1831 <muhammadsaleh1356@gmail.com>
2023-12-03 21:08:01 +0330

    creat post method

--- lib/services/dio_service.dart
+++ lib/services/dio_service.dart
@@ -1,9 +1,10 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' as dio_service;
 
 class DioService {
+  Dio dio = Dio();
   Future<dynamic> getMethod(String url) async {
-    Dio dio = Dio();
     dio.options.headers["content-Type"] = "application/json";
     return await dio
         .get(
@@ -12,6 +13,21 @@ class DioService {
     )
         .then((response) {
       log(response.toString());
+      return response;
+    });
+  }
+
+  Future<dynamic> postMethod(Map<String, dynamic> map, String url) async {
+    dio.options.headers["content-Type"] = "application/json";
+    return await dio
+        .post(url,
+            data: dio_service.FormData.fromMap(map),
+            options: Options(responseType: ResponseType.json, method: 'POST'))
+        .then((response) {
+      log(response.data.toString());
+      log(response.headers.toString());
+      log(response.statusCode.toString());
+
       return response;
     });
   }
